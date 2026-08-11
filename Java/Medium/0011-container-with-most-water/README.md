c906ca17e7779301dd1cfeb4cfe52a74a4dcdd19
sriram108-coder <kasukurthibhargav8@gmail.com>
2026-08-11 22:49:18 +0530

    feat(leetcode): solve #0011 Container With Most Water (Medium) [Java]

--- Java/Medium/0011-container-with-most-water/README.md
+++ Java/Medium/0011-container-with-most-water/README.md
@@ -10,13 +10,13 @@
 | Metric | Value |
 |---|---|
 | Language | Java |
-| Runtime | 5 ms |
-| Memory | 73.90 MB |
+| Runtime | 3900 ms |
+| Memory | 48.50 MB |
 | Submitted | 2026-07-08 |
 
 ## Repository Path
 
-`Java/Medium/0011-container-with-most-water/attempts/2060472751/solution.java`
+`Java/Medium/0011-container-with-most-water/attempts/2060455270/solution.java`
 
 ## Synced By
 
---
+++ Java/Medium/0011-container-with-most-water/attempts/2060455270/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int maxarea = 0; 
+        int minh = 0;
+        int w = 0;
+
+        for(int i =0; i<n; i++){
+            for(int j = n-1; j>i; j--){
+                w = j-i;
+                minh = Math.min(height[i],height[j]);
+                int area = minh * w;
+                maxarea = Math.max(maxarea,area);
+
+
+            }
+        }
+        return maxarea;
+    }
+}
