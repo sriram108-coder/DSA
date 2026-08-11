4f6e4b27a845ee76fee5bc634b987a01d65d611a
sriram108-coder <kasukurthibhargav8@gmail.com>
2026-08-11 22:47:37 +0530

    feat(leetcode): solve #0206 Reverse Linked List (Easy) [Java]

---
+++ Java/Easy/0206-reverse-linked-list/README.md
@@ -0,0 +1,23 @@
+# 0206. Reverse Linked List
+
+🟢 Easy
+
+## Topics
+`Linked List` `Recursion` 
+
+## Solution Information
+
+| Metric | Value |
+|---|---|
+| Language | Java |
+| Runtime | N/A |
+| Memory | 42.41 MB |
+| Submitted | 2026-07-16 |
+
+## Repository Path
+
+`Java/Easy/0206-reverse-linked-list/attempts/2069532922/solution.java`
+
+## Synced By
+
+[LCSync](https://github.com/sriram1224/LCSync) — Automated DSA Portfolio Engine 🚀
---
+++ Java/Easy/0206-reverse-linked-list/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+
+        ListNode prev = null;
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode current = head;
+
+        while(temp.next!=null){
+            current = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = current;
+        }
+        current.next = prev;
+        head = current;
+
+        return head;
+    }
+}
